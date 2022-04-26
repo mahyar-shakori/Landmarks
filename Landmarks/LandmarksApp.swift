bcda6d5fce1549d12d8acccdb48d08de48dd74ce
Mahyar <shakourimahyar@outlook.com>
2022-04-26 09:13:10 +0430

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by MahyarShakouri on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
